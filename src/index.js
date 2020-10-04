8cd58664fcc2812e2c738ad578d388b6986f2c7f
Serene Lo <serenelo@users-MacBook-Air.local>
2020-10-04 23:28:29 +0800

    added auth

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Amplify from './aws-amplify';
+import Amplify from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 
